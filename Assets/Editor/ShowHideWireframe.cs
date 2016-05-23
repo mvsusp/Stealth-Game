9addb8624bd94bf06cafe292a3a93c5f07a13791
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-23 08:09:40 -0400

    first comit

---
+++ Assets/Editor/ShowHideWireframe.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ShowHideWireframe : EditorWindow
+{
+    [MenuItem("Editor/Show WireFrame")]
+    public static void ShowWire()
+    {
+        ToggleWireframe(false);
+    }
+
+    [MenuItem("Editor/Show WireFrame", true)]
+    public static bool CheckShow()
+    {
+        return Selection.activeGameObject != null;
+    }
+
+    private static void ToggleWireframe(bool show)
+    {
+        foreach (var gameObject in Selection.gameObjects)
+        {
+            var rend = gameObject.GetComponent<Renderer>();
+            if (rend != null)
+            {
+                EditorUtility.SetSelectedWireframeHidden(rend, show);
+            }
+        }
+    }
+
+    [MenuItem("Editor/Hide WireFrame")]
+    public static void HideWire()
+    {
+        ToggleWireframe(true);
+    }
+
+    [MenuItem("Editor/Hide WireFrame", true)]
+    public static bool CheckHide()
+    {
+        return Selection.activeGameObject != null;
+    }
+}
---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour
+{
+    public float fadeSpeed = 2f;
+    public float highIntensity = 2f;
+    public float lowIntensity = 0.5f;
+    public float changeMargin = 0.2f;
+    public bool alarmOn;
+
+    private float targetIntensity;
+
+    void Awake()
+    {
+        GetComponent<Light>().intensity = 0;
+        targetIntensity = highIntensity;
+    }
+
+    void Update()
+    {
+        var light = GetComponent<Light>();
+        if (alarmOn)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            CheckTargetIntensity();
+        }
+        else
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void CheckTargetIntensity()
+    {
+        if (Mathf.Abs(targetIntensity - GetComponent<Light>().intensity) < changeMargin)
+        {
+            if (targetIntensity == highIntensity)
+            {
+                targetIntensity = lowIntensity;
+            }
+            else
+            {
+                targetIntensity = highIntensity;
+            }
+        }
+    }
+}
