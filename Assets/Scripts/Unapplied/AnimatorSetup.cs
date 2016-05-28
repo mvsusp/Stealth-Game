324b1815827cb5f0961a888468d4cfc7606d3ec9
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-28 11:13:42 -0400

    enemy animation

---
+++ Assets/Scripts/Unapplied/AnimatorSetup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorSetup {
+    public float speedDampTime = 0.1f;
+    public float angularSpeedDampTime = 0.7f;
+    public float angleResponseTime = 0.6f;
+
+    private Animator anim;
+    private HashIds hash;
+
+    public AnimatorSetup(Animator animator, HashIds hash)
+    {
+        anim = animator;
+        this.hash = hash;
+    }
+
+    public void Setup(float speed, float angle)
+    {
+        float angularSpeed = angle / angleResponseTime;
+        anim.SetFloat(hash.speedFloat, speed, speedDampTime, Time.deltaTime);
+        anim.SetFloat(hash.angularSpeedFloat, angularSpeed, angularSpeedDampTime, Time.deltaTime);
+    }
+}
