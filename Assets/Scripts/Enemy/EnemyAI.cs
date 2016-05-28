0b4494061f2f316418e61adaee3ded4c1c9a3cca
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-28 15:36:22 -0400

    enemy ai

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 5f;
+    public float chaseWaitTime = 5f;
+    public float patrolWaitTime = 1f;
+    public Transform[] patrolWayPoints;
+
+    private EnemySight enemySight;
+    private NavMeshAgent nav;
+    private Transform player;
+    private PlayerHealth playerHealth;
+    private LastPlayerSighting lastPlayerSighting;
+    private float chaseTimer;
+    private float patrolTimer;
+    private int wayPointIndex;
+
+    void Awake()
+    {
+        enemySight = GetComponent<EnemySight>();
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+    }
+
+    void Update()
+    {
+        if (enemySight.playerInSight && playerHealth.health > 0)
+        {
+            Shooting();
+        } else if (enemySight.personalLastSighting != lastPlayerSighting.resetPosition && playerHealth.health > 0)
+        {
+            Chasing();
+        } else
+        {
+            Patrolling();
+        }
+    }
+
+    void Shooting()
+    {
+        nav.Stop();
+    }
+
+    void Chasing()
+    {
+        var sightingDeltaPos = enemySight.personalLastSighting - transform.position;
+        if (sightingDeltaPos.sqrMagnitude > 4f)
+        {
+            nav.destination = enemySight.personalLastSighting;
+        }
+
+        nav.speed = chaseSpeed;
+
+        if (nav.remainingDistance < nav.stoppingDistance)
+        {
+            chaseTimer += Time.deltaTime;
+
+            if (chaseTimer > chaseWaitTime)
+            {
+                lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+                enemySight.personalLastSighting = lastPlayerSighting.resetPosition;
+                chaseTimer = 0;
+            }
+        }
+        else
+        {
+            chaseTimer = 0;
+        }
+    }
+
+    void Patrolling()
+    {
+        nav.speed = patrolSpeed;
+
+        if (nav.destination == lastPlayerSighting.resetPosition || nav.remainingDistance < nav.stoppingDistance)
+        {
+            patrolTimer += Time.deltaTime;
+
+            if (patrolTimer >= patrolWaitTime)
+            {
+                if (wayPointIndex == patrolWayPoints.Length-1)
+                {
+                    wayPointIndex = 0;
+                }
+                else
+                {
+                    wayPointIndex++;
+                }
+                patrolTimer = 0;
+            }
+        }
+        else
+        {
+            patrolTimer = 0;
+        }
+        nav.destination = patrolWayPoints[wayPointIndex].position;
+    }
+}
